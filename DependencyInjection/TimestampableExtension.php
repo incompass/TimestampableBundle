2f4396c2b753b860497e4f8f3466afb1576988bc
Joe Mizzi <themizzi@me.com>
2017-03-01 23:12:09 -0600

    removed service yml and added extension tests

--- DependencyInjection/TimestampableExtension.php
+++ DependencyInjection/TimestampableExtension.php
@@ -2,11 +2,10 @@
 
 namespace Incompass\TimestampableBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Incompass\TimestampableBundle\EventListener\TimestampableSubscriber;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * Class TimestampableExtension
@@ -22,10 +21,8 @@ class TimestampableExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__ . '/../Resources/config')
-        );
-        $loader->load('services.yml');
+        $definition = new Definition(TimestampableSubscriber::class);
+        $definition->addTag('doctrine.event_subscriber');
+        $container->setDefinition('timestampable.subscriber', $definition);
     }
 }
