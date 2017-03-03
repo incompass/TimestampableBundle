da4a5f08fde104b30a5d7774608b84e4cd2b876b
Joe Mizzi <themizzi@me.com>
2017-03-03 13:07:01 -0600

    Fixed events for cascading entities.

--- EventListener/TimestampableSubscriber.php
+++ EventListener/TimestampableSubscriber.php
@@ -3,9 +3,13 @@
 namespace Incompass\TimestampableBundle\EventListener;
 
 use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Event\OnFlushEventArgs;
 use Doctrine\ORM\Event\PreFlushEventArgs;
 use Incompass\TimestampableBundle\Entity\Timestampable;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class TimestampableListener
@@ -21,26 +25,8 @@ class TimestampableSubscriber implements EventSubscriber
     public function getSubscribedEvents()
     {
         return array(
-            'preFlush',
             'onFlush',
         );
-    }
-
-    /**
-     * @param PreFlushEventArgs $args
-     */
-    public function preFlush(PreFlushEventArgs $args)
-    {
-        $uow = $args->getEntityManager()->getUnitOfWork();
-
-        foreach ($uow->getScheduledEntityInsertions() as $insertion) {
-            if (in_array(Timestampable::class, class_uses($insertion))) {
-                /** @var Timestampable $insertion */
-                $now = new \DateTime(null, new \DateTimeZone('UTC'));
-                $insertion->setCreatedAt($now);
-                $insertion->setUpdatedAt($now);
-            }
-        }
     }
 
     /**
@@ -50,11 +36,28 @@ class TimestampableSubscriber implements EventSubscriber
     {
         $uow = $args->getEntityManager()->getUnitOfWork();
 
+        foreach ($uow->getScheduledEntityInsertions() as $insert) {
+            if (in_array(Timestampable::class, class_uses($insert))) {
+                /** @var Timestampable $update */
+                $now = new \DateTime(null, new \DateTimeZone('UTC'));
+                $insert->setCreatedAt($now);
+                $insert->setUpdatedAt($now);
+                $uow->recomputeSingleEntityChangeSet(
+                    $args->getEntityManager()->getClassMetadata(get_class($insert)),
+                    $insert
+                );
+            }
+        }
+
         foreach ($uow->getScheduledEntityUpdates() as $update) {
             if (in_array(Timestampable::class, class_uses($update))) {
                 /** @var Timestampable $update */
                 $now = new \DateTime(null, new \DateTimeZone('UTC'));
                 $update->setUpdatedAt($now);
+                $uow->recomputeSingleEntityChangeSet(
+                    $args->getEntityManager()->getClassMetadata(get_class($update)),
+                    $update
+                );
             }
         }
     }
